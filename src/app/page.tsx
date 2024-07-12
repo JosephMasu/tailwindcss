ef95380c247e34b7386a70fa125cfcc2301776ae
Joe Masu <josephmuhindo089@gmail.com>
2024-07-12 15:41:05 +0300

    spacing in tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      {/* bg */}
+      {/* <h1 className="bg-green-600 text-white">
+        Hello world!
+      </h1> */}
+      {/* border */}
+      {/* <h1 className="border-4 border-purple-500 text-black">
+        Hello world!
+      </h1> */}
+      {/* md queries */}
+      {/* <h1 className=" text-xs lg:text-3xl">
+        Hello Masu!
+      </h1> */}
+      {/* Space */}
+      <p className="mb-1">Masu Muhindo</p>
+      <p >Masu Muhindo</p>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,30 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      colors:{
+        vuejs:'#7932a8'
+      }
+    }
   },
   plugins: [],
 };
