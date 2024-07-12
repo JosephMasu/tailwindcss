4c6590fe716fba018cd8bd53cf6eaee11c2d2b09
Joe Masu <josephmuhindo089@gmail.com>
2024-07-13 01:26:46 +0300

    flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,25 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      {/* bg */}
-      {/* <h1 className="bg-green-600 text-white">
-        Hello world!
-      </h1> */}
-      {/* border */}
-      {/* <h1 className="border-4 border-purple-500 text-black">
-        Hello world!
-      </h1> */}
-      {/* md queries */}
-      {/* <h1 className=" text-xs lg:text-3xl">
-        Hello Masu!
-      </h1> */}
-      {/* Space */}
-      {/* <p className="mb-1">Masu Muhindo</p>
-      <p >Masu Muhindo</p> */}
+    // <div className="parent">
+    //   <div>Header</div>
 
-      {/* <h1 className="text-3xl underline
-       decoration-white decoration-wavy
-       decoration-1
-       underline-offset-4"
-       >Title</h1>
-      <h2 className="text-2xl">Title</h2>
-      <h3 className="text-0xl">Title</h3>
+    //   <div className="flex flex-col md:flex-row">
+    //     <div className="flex flex-unwrap w-full ">
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //       <div className="w-full grow">Side Bar</div>
+    //     </div>
+    //   </div>
+    // </div>
+  <div className="flex justify-evenly">
+  <div>01</div>
+  <div>02</div>
+  <div>03</div>
+</div>
 
-      <p className="text-3xl underline
-       decoration-white decoration-wavy
-       decoration-1
-       underline-offset-4">Masu Muhindo</p>
-      <p className="text-sm">Masu Muhindo</p> */}
-      <p className="whitespace-nowrap">Masu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu Muhindo</p>
-      <p className="break-words">Masu MuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasuMuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu MuhindoMasu Muhindo</p>
-    </div>
   );
 }
